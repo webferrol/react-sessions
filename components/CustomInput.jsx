ea5681239d95efc874ab06d84688e6f3b77f8163
webferrol <xurxo@webferrol.com>
2023-06-09 08:00:14 +0200

    Modificado CustomInput.jsx

--- components/CustomInput.jsx
+++ components/CustomInput.jsx
@@ -9,11 +9,16 @@ const CustomInput = forwardRef(({ textLabel, ...props }, ref) => {
   const id = useId()
   return (
     <>
-      <label
-        className='block mb-2 font-bold text-gray-700'
-        htmlFor={id}
-      >{textLabel}
-      </label>
+      {
+        textLabel &&
+          (
+            <label
+              className='block mb-2 font-bold text-gray-700'
+              htmlFor={id}
+            >{textLabel}
+            </label>
+          )
+      }
       <input
         className='block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500'
         id={id}
